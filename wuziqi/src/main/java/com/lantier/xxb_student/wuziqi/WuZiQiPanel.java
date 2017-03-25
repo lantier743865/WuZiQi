a7628556e3598da79405f9d26e3faaa72305a912
xxb <wuxiaolong@xuexibao.cn>
2017-03-25 21:30:28 +0800

    给代码加注释

--- wuziqi/src/main/java/com/lantier/xxb_student/wuziqi/WuZiQiPanel.java
+++ wuziqi/src/main/java/com/lantier/xxb_student/wuziqi/WuZiQiPanel.java
@@ -23,29 +23,41 @@ import java.util.List;
 
 public class WuZiQiPanel extends View {
     private static final String TAG = "WuZiQiPanel";
+    //棋盘的宽度和高度，为了正方形
     private int mPanelWidth;
+    //每一行的高度
     private float mLineHeight;
+    //设置棋盘为10*10的网格
     private int MAX_LINE = 10;
+    //五子棋，所以当达到5时说明有一方已经胜利
     private int MAX_COUNT_IN_LINE = 5;
 
+    //画笔
     private Paint mPaint = new Paint();
 
+    //白棋子和黑棋子的图片
     private Bitmap mWhitePiece;
     private Bitmap mBlackPiece;
 
+    //棋子只占网格宽度的3/4
     private float ratioPieceOfLineHeight = 3 * 1.0f /4;
 
     //白旗先手，当前轮到白旗
     private boolean mIsWhite = true;
+    //黑棋子集合和白棋子的集合
     private ArrayList<Point> mWhiteArray = new ArrayList<>();
     private ArrayList<Point> mBlackArray = new ArrayList<>();
 
+    //是否结束
     private boolean mIsGameOver = false;
+    //是否白旗为赢家
     private boolean mIsWhiteWinner = false;
+
 
 
     public WuZiQiPanel(Context context, @Nullable AttributeSet attrs) {
         super(context, attrs);
+        //初始化画笔，获取棋子的图片等
         init();
     }
 
@@ -66,6 +78,7 @@ public class WuZiQiPanel extends View {
         int widthMode = MeasureSpec.getMode(widthMeasureSpec);
         int heightSize = MeasureSpec.getSize(heightMeasureSpec);
         int heightMode = MeasureSpec.getMode(heightMeasureSpec);
+        //因为要绘制正方形，所以取宽和高的最小值
         int width = Math.min(widthSize,heightSize);
         //heightMode
         if (widthMode == MeasureSpec.UNSPECIFIED){
@@ -82,7 +95,9 @@ public class WuZiQiPanel extends View {
     protected void onSizeChanged(int w, int h, int oldw, int oldh) {
         super.onSizeChanged(w, h, oldw, oldh);
         mPanelWidth = w;
+        //总高度除以行数为每一行的高度
         mLineHeight = mPanelWidth * 1.0f /MAX_LINE;
+        //对旗子进行一个缩放，为每一个网格宽度的3/4
         int pieceWidth = (int) (mLineHeight * ratioPieceOfLineHeight);
        //对旗子的大小做限制
         mWhitePiece = Bitmap.createScaledBitmap(mWhitePiece,pieceWidth,pieceWidth,false);
@@ -91,6 +106,7 @@ public class WuZiQiPanel extends View {
 
     @Override
     public boolean onTouchEvent(MotionEvent event) {
+        //如果五子棋结束则不能再落字
         if (mIsGameOver){
             return false;
         }
@@ -99,33 +115,43 @@ public class WuZiQiPanel extends View {
             int x = (int) event.getX();
             int y = (int) event.getY();
             Point p = getValidPoint(x,y);
+            //判断是否这个地方已经落过子
             if (mWhiteArray.contains(p) || mBlackArray.contains(p)){
                 return false;
             }
+            //每次都把所下的棋子加进集合，绘制时检查是否结束
             if (mIsWhite){
                 mWhiteArray.add(p);
             } else {
                 mBlackArray.add(p);
             }
+            //每次都重新绘制
             invalidate();
+            //白棋子和黑棋子轮流绘制
             mIsWhite = !mIsWhite;
         }
+        //表明处理了touch事件
         return true;
     }
 
     private Point getValidPoint(int x, int y) {
+        //用计算后的int值更容易判断一个点的位置是否已经下过了，防止重复
         return new Point((int)(x / mLineHeight), (int)(y / mLineHeight));
     }
 
     @Override
     protected void onDraw(Canvas canvas) {
         super.onDraw(canvas);
+        //绘制棋盘
         drawBoard(canvas);
+        //绘制棋子
         drawPieces(canvas);
+        //检查是否结束
         checkGameOver();
     }
 
     private void checkGameOver() {
+        //每次绘制都检测白棋子和黑棋子是否结束
         boolean whiteWin = checkFiveInLine(mWhiteArray);
         boolean blackWin = checkFiveInLine(mBlackArray);
         if (whiteWin || blackWin){
@@ -137,6 +163,7 @@ public class WuZiQiPanel extends View {
     }
 
     private boolean checkFiveInLine(List<Point> points) {
+        //横向检测、纵向检测、左上方、左下方、右上方、右下方分别检测
         for (Point p : points){
             int x = p.x;
             int y = p.y;
@@ -265,6 +292,8 @@ public class WuZiQiPanel extends View {
         return  false;
     }
 
+    //以下两个完全是按照数学方法绘制，重点是每个线的x,y坐标，每个棋子的x，y坐标，大家看图绝对能看懂
+    //不懂可以加我qq635912159
     private void drawPieces(Canvas canvas) {
         for (int i = 0 ,n = mWhiteArray.size(); i < n; i++) {
             Point whitePoint = mWhiteArray.get(i);
@@ -291,6 +320,7 @@ public class WuZiQiPanel extends View {
             canvas.drawLine(y,startX,y,endX,mPaint);
         }
     }
+    //重来一局，清除数据
     public void  start(){
         mWhiteArray.clear();
         mBlackArray.clear();
@@ -298,6 +328,9 @@ public class WuZiQiPanel extends View {
         mIsWhiteWinner =false;
         invalidate();
     }
+
+    //数据保存恢复机制，防止屏幕旋转等销毁当前数据，所以每次保存当前数据
+    //重新创建时再绘制上次销毁时的局面
     private static final String INSTANCE = "instance";
     private static final String INSTANCE_GAME_OVER = "instance_game_over";
     private static final String INSTANCE_WHITE_ARRAY = "instance_white_array";
